d8ab161d1b443fa9fa0ced956204ddaf2499f120
bobcermak <bohuslavcermak62@gmail.com>
2025-01-22 14:28:10 +0100

    update

--- html_gen.py
+++ html_gen.py
@@ -22,15 +22,36 @@ class Html:
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QRCode</title>
+    <style>
+        body {{
+            height: 100vh;
+            margin: 0;
+            font-family: Arial, sans-serif;
+        }}
+
+        .container {{
+            margin: 0 auto;
+            text-align: center;
+        }}
+
+        img {{
+            display: block;
+            max-width: 100%;
+            height: auto;
+            margin: 0 auto;
+            padding: 0;
+        }}
+    </style>
 </head>
-<body style="height: 100vh; margin: 0; font-family: Arial, sans-serif;">
-    <div style="margin: 0 auto; text-align: center;">
+<body>
+    <div class="container">
         <h1>QRCode</h1>
-        <img src="{path_img}{path_url}.png" alt="QRCode" width="400" height="400" style="display: block; max-width: 100%; height: auto; margin: 0 auto; padding: 0;">
+        <img src="{path_img}{path_url}.png" alt="QRCode" width="400" height="400">
         <a href="{self.fullUrl}" target="_blank">{self.fullUrl}</a>
         <p>The QR code has been downloaded to the <a href="{path_img}{path_url}.png" target="_blank">{path_img}{path_url}.png</a>.</p>
     </div>
 </body>
+</html>
 </html>
 """
         with open(f"{path_index}{our_url}.html", "w") as file:
